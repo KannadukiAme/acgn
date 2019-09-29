0fa54773228308850e1f653293aac7ddab7d283d
KannadukiAme <misuzunagisa@gmail.com>
2019-09-29 14:58:23 +0800

    feat: add 404 page

---
+++ theme/layouts/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <img class="not-found" src="/404-2.png" />
+</template>
+
+<script>
+export default {
+  name: '404'
+}
+</script>
+
+<style lang="stylus">
+.not-found
+  object-fit cover
+  width 980px
+  height 800px
+  margin 0 auto
+  display block
+</style>
